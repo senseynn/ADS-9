8d5fcbc7b57728a7fc49f38945fb76010506e212
senseynn <163774648+senseynn@users.noreply.github.com>
2026-05-29 14:15:30 +0300

    Update tree.h

--- include/tree.h
+++ include/tree.h
@@ -2,4 +2,34 @@
 #ifndef INCLUDE_TREE_H_
 #define INCLUDE_TREE_H_
 
+#include <vector>
+
+class PMTree {
+public:
+  struct TreeNode {
+    char symbol;
+    std::vector<TreeNode*> branches;
+    explicit TreeNode(char val) : symbol(val) {}
+  };
+
+  TreeNode* root_node;
+  std::vector<char> source_elements;
+
+  explicit PMTree(const std::vector<char>& input_data);
+  ~PMTree();
+
+  PMTree(const PMTree&) = delete;
+  PMTree& operator=(const PMTree&) = delete;
+
+private:
+  TreeNode* BuildSubtree(const std::vector<char>& remaining);
+  void CleanupTree(TreeNode* node_ptr);
+};
+
+std::vector<std::vector<char>> GetAllPermutations(PMTree& tree_obj);
+std::vector<char> GetPermutationByIndexSlow(PMTree& tree_obj, int index);
+std::vector<char> GetPermutationByIndexFast(PMTree& tree_obj, int index);
+
+size_t CalculateFactorial(int value);
+
 #endif  // INCLUDE_TREE_H_
